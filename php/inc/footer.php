8b2b4380b557a2de8b14bf89ba3a8c6d613b2156
atelierbram <bramdeh@gmail.com>
2013-12-18 23:17:10 +0100

    added link to repo

--- php/inc/footer.php
+++ php/inc/footer.php
@@ -2,6 +2,7 @@
 
    <footer class="footer">
 
-    <p><small>&copy; Copyright <?php //if ($thisAuthor!="") echo "$thisAuthor"; ?> <a href="http://atelierbramdehaan.nl">Bram de Haan</a> 2012 - <?php echo $thisyear; ?> <a href="<?php if ($thislicenseLink!="") echo $thislicenseLink; ?>"><?php if ($thisLicense!="") echo $thisLicense; ?></a></small></p> 
+    <p><small>&copy; Copyright <?php //if ($thisAuthor!="") echo "$thisAuthor"; ?> <a href="http://atelierbramdehaan.nl">Bram de Haan</a> 2012 - <?php echo $thisyear; ?> <a href="<?php if ($thislicenseLink!="") echo $thislicenseLink; ?>"><?php if ($thisLicense!="") echo $thisLicense; ?></a></small>
+<small><a href="https://github.com/atelierbram/houtje-touwtje">Fork this on Github</a></small></p>
   </footer>
 
