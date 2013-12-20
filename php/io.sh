f77ebfa1963f42496cdb79601a5a23c4d4290773
atelierbram <bramdeh@gmail.com>
2013-12-20 22:59:50 +0100

    edit

--- php/io.sh
+++ php/io.sh
@@ -4,10 +4,10 @@
 write_to_file()
 {
 
- curl "http://localhost/~bram/php/houtje-touwtje/php/index.php" > "../index.html"
- curl "http://localhost/~bram/php/houtje-touwtje/php/lab/index.php" > "../lab/index.html"
- curl "http://localhost/~bram/php/houtje-touwtje/php/about/index.php" > "../about/index.html"
- curl "http://localhost/~bram/php/houtje-touwtje/php/contact/index.php" > "../contact/index.html"
+ curl "http://localhost/~www/houtje-touwtje/php/index.php" > "../index.html"
+ curl "http://localhost/~www/houtje-touwtje/php/lab/index.php" > "../lab/index.html"
+ curl "http://localhost/~www/houtje-touwtje/php/about/index.php" > "../about/index.html"
+ curl "http://localhost/~www/houtje-touwtje/php/contact/index.php" > "../contact/index.html"
  }
 
 # execute it
