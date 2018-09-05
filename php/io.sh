7d69ed74a590bf3c100e4e67a9333f792b52ada9
atelierbram <bramdeh@gmail.com>
2018-09-05 10:59:39 +0200

    link stylesheets fix to https

--- php/io.sh
+++ php/io.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # write to file
 write_to_file()
 {
 
- curl "http://localhost/~www/houtje-touwtje/php/index.php" > "../index.html"
- curl "http://localhost/~www/houtje-touwtje/php/lab/index.php" > "../lab/index.html"
- curl "http://localhost/~www/houtje-touwtje/php/about/index.php" > "../about/index.html"
- curl "http://localhost/~www/houtje-touwtje/php/contact/index.php" > "../contact/index.html"
+ curl "http://localhost/~bram/php/houtje-touwtje/php/index.php" > "../index.html"
+ curl "http://localhost/~bram/php/houtje-touwtje/php/lab/index.php" > "../lab/index.html"
+ curl "http://localhost/~bram/php/houtje-touwtje/php/about/index.php" > "../about/index.html"
+ curl "http://localhost/~bram/php/houtje-touwtje/php/contact/index.php" > "../contact/index.html"
  }
 
 # execute it
